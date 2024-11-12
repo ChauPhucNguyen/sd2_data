407bcadcf04b2f4778f05afed635cb016b8fa85b
ChauPhucNguyen <cdn9680@mavs.uta.edu>
2024-11-12 17:38:10 -0600

    added more pages

--- app.py
+++ app.py
@@ -7,7 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projectFiles')
+def projectFiles():
+    return render_template('projectFiles.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/videoDemo')
+def videoDemo():
+    return render_template('videoDemo.html')
 
 @app.route('/data')
 def get_data():
@@ -15,6 +31,7 @@ def get_data():
         "speed": random.uniform(0.5,1.5),
         "steeringAngle": random.uniform(-30,30),
         "batteryLevel": random.uniform(0.0, 100.0),
+        "batteryConsumption" : random.uniform(0.01,0.05),
     }
     return jsonify(data)
 
